54d6076adb1b095c624ea761d3b4de32f5cef774
Your Name <you@example.com>
2022-04-01 08:24:31 +0530

    Person details

---
+++ com/bl/Addressbook.java
@@ -0,0 +1,13 @@
+package com.bl;
+
+public class Addressbook 
+{
+	
+		public static void main(String[] args) 
+		{
+			System.out.println("Welcome to Address Book Program");
+		}
+
+	
+
+}
---
+++ com/bl/Person.java
@@ -0,0 +1,15 @@
+package com.bl;
+
+public class Person
+{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zipCode;
+	private long mobileNumber;
+	private String emailId;
+	
+
+}
