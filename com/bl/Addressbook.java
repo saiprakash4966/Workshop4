b0a18e9c09d56aa2b6374cf0b2ddd72e3c4bab41
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-04-01 08:57:04 +0530

    Update Addressbook.java

--- com/bl/Addressbook.java
+++ com/bl/Addressbook.java
@@ -1,50 +1,15 @@
 package com.bl;
 
+import java.util.Scanner;
+/**
+ * Class to create implementation methods for the address book.
+ * [1]-Method to add contacts to the address book.s
+ * @author Sai
+ *
+ */
+
+public class Addressbook
 {
-	
-
-		/**
-		 * [1]-Method to add contacts to the address book.
-		 * We are taking the input from the console and using the constructor to create object and assign the value.
-		 */
-		public void addContact() {
-
-			Scanner scan = new Scanner(System.in);
-
-			System.out.print(" Please enter the first name: ");
-			String firstName = scan.next();
-
-			System.out.print(" Please enter the last name: ");
-			String lastName = scan.next();
-
-			System.out.print(" Please enter the address: ");
-			String address = scan.next();
-
-			System.out.print(" Please enter the city: ");
-			String city = scan.next();
-
-			System.out.print(" Please enter the state: ");
-			String state = scan.next();
-
-			System.out.print(" Please enter the zip: ");
-			int zip = scan.nextInt();
-
-			System.out.print(" Please enter the mobile number: ");
-			long mobileNumber = scan.nextLong();
-
-			System.out.print(" Please enter the email: ");
-			String email = scan.next();
-
-			// Creating a new object of the contact class with the parameter values filled by user
-			Person P = new Person(firstName, lastName, address, city, state, zip, mobileNumber, email);
-			System.out.println(P);
-
-		
-	}
-
-}
-
-public class Addressbook {
 	
 
 		/**
