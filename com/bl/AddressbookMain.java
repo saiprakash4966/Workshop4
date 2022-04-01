0127c43022c6106bb616db2f35a45d27619e607d
saiprakash4966 <97298373+saiprakash4966@users.noreply.github.com>
2022-04-01 08:55:20 +0530

    Update AddressbookMain.java

--- com/bl/AddressbookMain.java
+++ com/bl/AddressbookMain.java
@@ -7,7 +7,16 @@ public class AddressbookMain
 		{
 			System.out.println("Welcome to Address Book Program");
 		}
-
-	
+		
+		Addressbook AB = new Addressbook();
+		{
+		System.out.println("Enter total no of Contacts to add.");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		for(int i=0;i<num;i++)
+		{
+		AB.addContact();
+		}
+	}
 
 }
